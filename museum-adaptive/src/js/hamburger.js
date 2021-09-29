02f785f361ac4de3bd9430e3a28c9a3938495e99
Liubomyr86 <demianchukliubomyr@gmail.com>
2021-09-29 21:23:23 +0200

    feat: make a page layout for 1024px

---
+++ museum-adaptive/src/js/hamburger.js
@@ -0,0 +1,27 @@
+const MENU_BUTTON = document.querySelector('.hamburger');
+const MENU = document.querySelector('.navigation');
+const NAV_LINKS = document.querySelectorAll('.navigation__link');
+const welcomeContent = document.querySelector('.welcome__content')
+
+const hamburgerListner = () => {
+  const  menuOpen = () => {
+    MENU_BUTTON.classList.toggle('open')
+    MENU.classList.toggle('open')
+    welcomeContent.classList.toggle('hiden')
+  }
+
+  const menuClose = () => {
+    MENU_BUTTON.classList.remove('open')
+    MENU.classList.remove('open')
+    welcomeContent.classList.remove('hiden')
+  }
+
+  MENU_BUTTON.addEventListener('click', menuOpen);
+  NAV_LINKS.forEach((link) => {
+    link.addEventListener('click', menuClose);
+  });
+}
+
+
+
+export default hamburgerListner();
