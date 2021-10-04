81ff4c207e71c8f00ac592a7e4fc2d1baf50000f
Liubomyr86 <demianchukliubomyr@gmail.com>
2021-10-05 00:40:34 +0200

    fix: minor fix

--- museum-adaptive/src/js/hamburger.js
+++ museum-adaptive/src/js/hamburger.js
@@ -2,19 +2,23 @@ const MENU_BUTTON = document.querySelector('.hamburger');
 const MENU = document.querySelector('.navigation-header');
 const NAV_LINKS = document.querySelectorAll('.navigation__link');
 const SOC_LINKS = document.querySelectorAll('.social__link');
-const welcomeContent = document.querySelector('.welcome__content')
+const welcomeContent = document.querySelector('.welcome__content');
+const main = document.querySelector('.main');
+
 
 const hamburgerListner = () => {
   const  menuOpen = () => {
+    console.log(MENU)
     MENU_BUTTON.classList.toggle('open')
     MENU.classList.toggle('open')
     welcomeContent.classList.toggle('hiden')
+    console.log(MENU)
   }
 
   const menuClose = () => {
-    MENU_BUTTON.classList.remove('open')
-    MENU.classList.remove('open')
-    welcomeContent.classList.remove('hiden')
+      MENU_BUTTON.classList.remove('open')
+      MENU.classList.remove('open')
+      welcomeContent.classList.remove('hiden')
   }
 
   MENU_BUTTON.addEventListener('click', menuOpen);
@@ -24,7 +28,10 @@ const hamburgerListner = () => {
   SOC_LINKS.forEach((link) => {
     link.addEventListener('click', menuClose);
   });
+  main.addEventListener('click', menuOpen)
 }
+
+
 
 
 
